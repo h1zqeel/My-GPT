17c19e55cd979b9246d4cf28cced9b9ddb554b4d
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-07-06 00:06:08 +0500

    move authOptions to utils

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,7 @@
 import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import type { NextAuthOptions } from 'next-auth';
 
-export const authOptions : NextAuthOptions = {
-	session:{
-		strategy: 'jwt'
-	},
-	providers: [
-		CredentialsProvider({
-			name: 'Login',
-			credentials: {
-				username: { label: 'Username', type: 'text', placeholder: 'username' },
-				password: { label: 'Password', type: 'password', placeholder: 'password' }
-			},
-			async authorize(credentials, req) {
-				const res = await fetch(`${process.env.BASE_URL}/login/api`, {
-					method: 'POST',
-					body: JSON.stringify(credentials),
-					headers: { 'Content-Type': 'application/json' }
-				});
-				const user = await res.json();
-				if (res.ok && user) {
-					return user;
-				}
-				return null;
-			}
-		})
-	],
-	pages: {
-		signIn: '/login',
-		error: '/error'
-	}
-};
+import { authOptions } from '@/utils/authOptions';
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
---
+++ utils/authOptions.ts
@@ -0,0 +1,33 @@
+import CredentialsProvider from 'next-auth/providers/credentials';
+import type { NextAuthOptions } from 'next-auth';
+
+export const authOptions : NextAuthOptions = {
+	session:{
+		strategy: 'jwt'
+	},
+	providers: [
+		CredentialsProvider({
+			name: 'Login',
+			credentials: {
+				username: { label: 'Username', type: 'text', placeholder: 'username' },
+				password: { label: 'Password', type: 'password', placeholder: 'password' }
+			},
+			async authorize(credentials, req) {
+				const res = await fetch(`${process.env.BASE_URL}/login/api`, {
+					method: 'POST',
+					body: JSON.stringify(credentials),
+					headers: { 'Content-Type': 'application/json' }
+				});
+				const user = await res.json();
+				if (res.ok && user) {
+					return user;
+				}
+				return null;
+			}
+		})
+	],
+	pages: {
+		signIn: '/login',
+		error: '/error'
+	}
+};
