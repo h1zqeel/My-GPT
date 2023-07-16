2a124070a6e7498b79aae9e24dea841815c0b352
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-07-16 23:21:20 +0500

    APIs for Messages

---
+++ app/chats/[id]/api/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from 'next/server';
+import db from '@/prisma/db';
+import { createEdgeRouter } from 'next-connect';
+import { chatBelongsToUser } from '@/utils/customMiddlewares';
+
+interface RequestContext {
+	params: {
+		id: number;
+	};
+}
+const router = createEdgeRouter<NextRequest, RequestContext>();
+
+router
+	.use(chatBelongsToUser)
+	.get(async(req: NextRequest, { params } : {params: {id: number | string}})=>{
+		const { id: chatId } = params;
+		const messages = await db.message.findMany({
+			where: {
+				chatId: Number(chatId)
+			}
+		});
+
+		return NextResponse.json(
+			{
+				ok: true,
+				messages
+			},
+			{ status: 200 }
+		);
+	})
+	.post(async(req: NextRequest, { params } : {params: {id: number | string}}) => {
+		const { id: chatId } = params;
+		const { text } = await req.json();
+
+		if(!text) {
+			return NextResponse.json(
+				{
+					ok: false,
+					error: 'Text is required'
+				},
+				{ status: 400 }
+			);
+		}
+
+		const message = await db.message.create({
+			data: {
+				text,
+				chatId: Number(chatId),
+				sender: 'User'
+			}
+		});
+
+
+		return NextResponse.json(
+			{
+				ok: true,
+				message
+			},
+			{ status: 200 }
+		);
+	})
+;
+
+async function handler(request: NextRequest, ctx: RequestContext) {
+	return router.run(request, ctx);
+}
+
+export { handler as GET, handler as POST };
---
+++ utils/customMiddlewares.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getUserSession } from './session';
+import db from '@/prisma/db';
+
+export const chatBelongsToUser = async(req : NextRequest, { params } : {params: {id: number}}, next : Function) => {
+	const { id: userId } = await getUserSession(req);
+	const { id: chatId } = params;
+	const chat = await db.chat.findUnique({
+		where: {
+			id: Number(chatId)
+		}
+	});
+
+	if(chat?.creatorId !== userId) {
+		return NextResponse.json({
+			ok: false,
+			error: 'You do not have permission to access this chat'
+		}, { status : 403 });
+	} else {
+		return next();
+	}
+};
+
