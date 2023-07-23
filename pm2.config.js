e709e1f440d96c248e94e60e272453d94d21799c
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-07-23 15:16:05 +0500

    run max instances for pm2 (#9)

--- pm2.config.js
+++ pm2.config.js
@@ -4,6 +4,8 @@ module.exports = {
 			name: 'my-gpt',
 			script: 'node_modules/next/dist/bin/next',
 			args: 'start',
+			instances : 'max',
+			exec_mode : 'cluster',
 			env_production: {
 				APP_ENV: 'prod'
 			}
