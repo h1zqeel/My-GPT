13fd71f2d0a4eac0ba98365a70ca03411ac3c277
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-07-27 03:14:07 +0500

    remove unnecessary router usage

--- components/Sidebar/subcomponents/SidebarElement.tsx
+++ components/Sidebar/subcomponents/SidebarElement.tsx
@@ -1,20 +1,18 @@
 'use client';
 
 import { TSidebarElement } from '@/types/Sidebar';
-import { useRouter } from 'next/navigation';
 import { useAppSelector } from '@/redux/hooks';
+import Link from 'next/link';
 
 const SidebarElement = ({ Icon = '', text = '', page = 'chats', id }: TSidebarElement) => {
 	const selectedChatId = useAppSelector(({ selectedChatReducer }) => selectedChatReducer.chatId);
-	const router = useRouter();
-	const redirect = () => {
-		return router.push(`/${page}/${id}`);
-	};
 	return (
-		<div className={`transition-colors duration-150 flex items-center justify-between m-2 rounded-md p-2 cursor-pointer hover:bg-secondary hover:text-black w-64 overflow-hidden ${id === selectedChatId ? 'bg-primary text-black': ''}`} onClick={redirect}>
-			<p>{text}</p>
-			{Icon && <div className='float-right'><Icon id={id}/></div>}
-		</div>
+		<Link href={`/${page}/${id}`}>
+			<div className={`transition-colors duration-150 flex items-center justify-between m-2 rounded-md p-2 cursor-pointer hover:bg-secondary hover:text-black w-64 overflow-hidden ${id === selectedChatId ? 'bg-primary text-black': ''}`}>
+				<p>{text}</p>
+				{Icon && <div className='float-right'><Icon id={id}/></div>}
+			</div>
+		</Link>
 	);
 };
 
