fe841e9dd2d9dec6e13ea23bcbc28c6707b9034a
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-07-28 01:07:12 +0500

    a few fixes

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export const middleware = async(req: NextRequest) => {
 		const tokenClaims = await decryptAndVerifyToken(token);
 		if(tokenClaims) {
 			if(!tokenClaims?.payload?.username) {
-				await axios.get('api/auth/logout');
+				await axios.get('/api/auth/logout');
 
 				return NextResponse.redirect(new URL('/login', req.url));
 			} else {
--- redux/features/sessionSlice.ts
+++ redux/features/sessionSlice.ts
@@ -15,7 +15,7 @@ const initialState = {
 export const getSession = createAsyncThunk(
 	'session/getSession',
 	async(thunkAPI) => {
-		const response = await axios.get('api/auth');
+		const response = await axios.get('/api/auth');
 		return response.data;
 	});
 export const session = createSlice({
