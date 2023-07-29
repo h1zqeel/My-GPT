ac0cd4af5f34dbb017b75a7c3fa6c049c9c9929d
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-07-29 07:06:33 +0500

    fix cache key

--- prisma/db.ts
+++ prisma/db.ts
@@ -9,7 +9,7 @@ const cacheMiddleware = createPrismaRedisCache({
 	models: [
 		{ model: 'Chat', cacheTime: 200, cacheKey:  `${process.env.TOKEN_NAME}_chat` },
 		{ model: 'Message', cacheTime: 200, cacheKey: `${process.env.TOKEN_NAME}_message` },
-		{ model: 'User', cacheTime: 400, cacheKey: `${process.env.TOKEN_NAME}_message` }
+		{ model: 'User', cacheTime: 400, cacheKey: `${process.env.TOKEN_NAME}_user` }
 	],
 	storage: { type: 'redis', options: { client: redis, invalidation: { referencesTTL: 300 }, log: console } },
 	cacheTime: 300,
