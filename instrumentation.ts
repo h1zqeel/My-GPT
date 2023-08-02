5bf1af5d007ed43fc5fd4fb6b7f89569bbf941df
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-08-02 14:06:28 +0500

    Revert "add Instrumentation"
    This reverts commit c4be484c3667a44b224d9f95603b5c7ac332b340.

--- instrumentation.ts
+++
@@ -1,7 +0,0 @@
-import db from './prisma/db';
-
-export async function register() {
-	console.time('prisma:connect');
-	await db.$connect();
-	console.timeEnd('prisma:connect');
-}
--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@ const nextConfig = {
 				pathname: '/*'
 			}
 		]
-	},
-	experimental: {
-		instrumentationHook: true
 	}
 };
 
