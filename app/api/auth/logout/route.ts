a941c9f6dd39a313dc15aa9010569cca43c1c410
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-08-03 02:39:24 +0500

    session expiry

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -4,7 +4,8 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(req: NextRequest) {
 	const response = NextResponse.redirect(new URL('/login', req.url));
 	const sessionId = req.cookies.get(process.env.TOKEN_NAME)?.value;
-	await cache.del(sessionId as string);
+	await cache.hdel(`${process.env.TOKEN_NAME}-sessions`, sessionId as string);
+	await cache.hdel(`${process.env.TOKEN_NAME}-session-last-used`, sessionId as string);
 	response.cookies.delete(process.env.TOKEN_NAME);
 	return response;
 }
--- utils/session.ts
+++ utils/session.ts
@@ -1,4 +1,5 @@
 import cache from './cache';
+import moment from 'moment-timezone';
 import { TUser } from '@/types/User';
 import { NextRequest, NextResponse } from 'next/server';
 import { v4 as uuidv4 } from 'uuid';
@@ -32,14 +33,14 @@ export const generateSession = async(user: TUser, { userSessionId, redirect, url
 		providers: user.providers
 	};
 
-	await cache.set(sessionId, JSON.stringify(sessionData), { ex: 60 * 60 * 24 * 1 });
+	await cache.hset(`${process.env.TOKEN_NAME}-sessions`, { [sessionId as string]: JSON.stringify(sessionData) });
+	await cache.hset(`${process.env.TOKEN_NAME}-session-last-used`, { [sessionId as string]: moment().unix() });
 
 	response.cookies.set({
 		name: process.env.TOKEN_NAME,
 		value: sessionId,
 		httpOnly: true,
-		sameSite: 'lax',
-		maxAge: 60 * 60 * 24 * 1
+		sameSite: 'lax'
 	});
 
 	return response;
@@ -55,7 +56,20 @@ export const getUserSession =async({ req, sessionId }:{req?: NextRequest, sessio
 		return null;
 	}
 
-	const session = await cache.get(userSessionId as string);
+	const sessionLastAccessed = await cache.hget(`${process.env.TOKEN_NAME}-session-last-used`, userSessionId as string);
+	if(sessionLastAccessed) {
+		if(moment().diff(moment.unix(sessionLastAccessed as number), 'hours') > 24) {
+			await cache.hdel(`${process.env.TOKEN_NAME}-sessions`, userSessionId as string);
+			await cache.hdel(`${process.env.TOKEN_NAME}-session-last-used`, userSessionId as string);
+			return null;
+		}
+	}
+	const session = await cache.hget(`${process.env.TOKEN_NAME}-sessions`, userSessionId as string);
+
+	if(session) {
+		await cache.hset(`${process.env.TOKEN_NAME}-session-last-used`, { [userSessionId as string]: moment().unix() });
+	}
+
 	if(!session) {
 		return null;
 	}
