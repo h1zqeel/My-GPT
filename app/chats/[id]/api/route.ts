f839d094ec59b357e37b09ee90a2cc8138507c34
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-08-27 22:52:53 +0500

    add chatBelongsToUser middelware to delete api

--- app/chats/[id]/api/route.ts
+++ app/chats/[id]/api/route.ts
@@ -1,37 +1,48 @@
 import db from '@/prisma/db';
-import { getUserSession } from '@/utils/session';
 import { NextRequest, NextResponse } from 'next/server';
+import { createEdgeRouter } from 'next-connect';
+import { chatBelongsToUser } from '@/utils/customMiddlewares';
 import { errors } from '@/constants';
+import { getUserSession } from '@/utils/session';
 interface RequestContext {
 	params: {
 		id: number | string;
 	};
 }
+const router = createEdgeRouter<NextRequest, RequestContext>();
 
-export async function DELETE(req: NextRequest, { params } : RequestContext) {
-	const user = await getUserSession({ req });
-	const { id: chatId } = params;
+router
+	.use(chatBelongsToUser)
+	.delete(async(req: NextRequest, { params } : RequestContext) => {
+		const user = await getUserSession({ req });
+		const { id: chatId } = params;
 
+		if(chatId) {
+			const chat = await db.chat.update({
+				data: {
+					archived: true
+				},
+				where: {
+					id: Number(chatId),
+					creatorId: user?.id
+				}
+			});
 
-	if(chatId) {
-		const chat = await db.chat.update({
-			data: {
-				archived: true
-			},
-			where: {
-				id: Number(chatId),
-				creatorId: user?.id
-			}
-		});
+			return NextResponse.json(
+				{ ok: true, chat },
+				{ status: 200 }
+			);
+		} else {
+			return NextResponse.json(
+				{ ok: false, error: errors.INVALID_REQUEST },
+				{ status: 400 }
+			);
+		}
+	});
+;
 
-		return NextResponse.json(
-			{ ok: true, chat },
-			{ status: 200 }
-		);
-	} else {
-		return NextResponse.json(
-			{ ok: false, error: errors.INVALID_REQUEST },
-			{ status: 400 }
-		);
-	}
+async function handler(request: NextRequest, ctx: RequestContext) {
+	return router.run(request, ctx);
 }
+
+export { handler as DELETE };
