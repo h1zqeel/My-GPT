fe498b032d954dddcb48c6ddac89e6728b3fd19c
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-09-03 01:31:35 +0500

    migrate

--- db/connection.ts
+++ db/connection.ts
@@ -1,7 +1,13 @@
 import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
+import { migrate } from 'drizzle-orm/neon-serverless/migrator';
+
 
 const pool = new Pool({ connectionString: 'postgres://hxzqll:iQqASceB4u9s@ep-late-pond-26867545-pooler.eu-central-1.aws.neon.tech/my-gpt' });
 const db = drizzle(pool);
+
+migrate(db, { migrationsFolder: './db/drizzle' }).then(()=>{
+	console.log('Migrations done');
+});
 
 export default db;
