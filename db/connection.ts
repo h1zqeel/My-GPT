cd9fdff9336ac58846d09892821994c7f3c0a70e
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-09-03 03:46:45 +0500

    added scripts for migrations and generation

--- db/connection.ts
+++ db/connection.ts
@@ -1,13 +1,7 @@
 import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
-import { migrate } from 'drizzle-orm/neon-serverless/migrator';
-
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const db = drizzle(pool);
-
-migrate(db, { migrationsFolder: 'db/drizzle' }).then(()=>{
-	console.log('Migrations done');
-});
 
 export default db;
