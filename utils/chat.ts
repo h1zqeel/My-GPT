64240582dc3966fa5927b75dd92a93f0371560de
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-09-03 14:40:05 +0500

    update cache key

--- utils/chat.ts
+++ utils/chat.ts
@@ -4,17 +4,17 @@ import { chats as chatsModel } from '@/db/schema';
 import { sql } from 'drizzle-orm';
 
 export const getChats = async(creatorId : number) => {
-	const chatsFromCache = await fetchData({ key: `chats_${creatorId}` });
+	const chatsFromCache = await fetchData({ key: `${process.env.TOKEN_NAME}::chats::${creatorId}` });
 	if(chatsFromCache) {
 		return chatsFromCache;
 	}
 	const chats = await db.select().from(chatsModel)
 		.where(sql`${chatsModel.creatorId} = ${creatorId} AND ${chatsModel.archived} = false`);
-	await createData({ key: `chats_${creatorId}`, value: chats });
+	await createData({ key: `${process.env.TOKEN_NAME}::chats::${creatorId}`, value: chats });
 
 	return chats;
 };
 
 export const invalidateChatsCache = async(creatorId : number) => {
-	await createData({ key: `chats_${creatorId}`, value: null });
+	await createData({ key: `${process.env.TOKEN_NAME}::chats::${creatorId}`, value: null });
 };
