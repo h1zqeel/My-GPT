6965de4fbdf5cebafd22b636926404abc9e149b2
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-09-03 05:30:09 +0500

    Revert "use http connection"
    This reverts commit ba4cca3ca3681bffa10a7732aa3fcb0f79cbc40c.

--- db/connection.ts
+++ db/connection.ts
@@ -1,9 +1,7 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon, neonConfig } from '@neondatabase/serverless';
+import { Pool } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 
-neonConfig.fetchConnectionCache = true;
-
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const db = drizzle(pool);
 
 export default db;
