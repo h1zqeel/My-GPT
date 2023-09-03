95e400cec7262ba2a42a5b4fe47484d2f1112f16
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-09-03 17:55:22 +0500

    drizzle-pool (#75)

--- db/connection.ts
+++ db/connection.ts
@@ -1,9 +1,10 @@
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Pool } from '@neondatabase/serverless';
 import { neon, neonConfig } from '@neondatabase/serverless';
 
 neonConfig.fetchConnectionCache = true;
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const db = drizzle(pool);
 
 export default db;
