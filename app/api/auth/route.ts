0bc9645b6dd8476a62203854b7301320ddd6d8ee
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-09-04 12:13:28 +0500

    hfa

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,4 +1,6 @@
+import db from '@/db/connection';
 import { getUserSession } from '@/utils/session';
+import { sql } from 'drizzle-orm';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const runtime = 'edge';
@@ -6,6 +8,7 @@ export const preferredRegion = 'fra1';
 
 export async function GET(req: NextRequest) {
 	const sessionId = req.cookies.get(process.env.TOKEN_NAME)?.value;
+	await db.execute(sql`SELECT 1;`);
 	if(sessionId) {
 		const session = await getUserSession({ sessionId });
 		return NextResponse.json({ user: session });
