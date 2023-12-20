e9bbe9d11c3457bd812431cc10a85a03c45c8476
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-12-20 16:57:59 +0500

    fixes

--- app/chats/api/route.ts
+++ app/chats/api/route.ts
@@ -7,7 +7,7 @@ import { eq } from 'drizzle-orm';
 import { getChats, invalidateChatsCache } from '@/utils/chat';
 
 export const runtime = 'edge';
-export const preferredRegion = 'fra1';
+export const preferredRegion = 'bom1';
 
 export async function POST(req: NextRequest) {
 	const user = await getUserSession({ req });
