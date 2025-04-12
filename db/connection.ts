71d6a83d90d656ac0634d0c188933ee62a20ddcb
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2025-04-13 01:32:44 +1000

    Fix Fonts (#254)
    * fix fonts
    
    * .
    
    * fix versions
    
    * fix

--- db/connection.ts
+++ db/connection.ts
@@ -3,7 +3,20 @@ import { neon, neonConfig } from '@neondatabase/serverless';
 
 neonConfig.fetchConnectionCache = true;
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql);
+const connectionString = process.env.DATABASE_URL!;
+
+if (process.env.NODE_ENV === 'development') {
+	neonConfig.fetchEndpoint = (host) => {
+		const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
+		console.log({ host, protocol, port });
+		return `${protocol}://${host}:${port}/sql`;
+	};
+	const connectionStringUrl = new URL(connectionString);
+	neonConfig.useSecureWebSocket = connectionStringUrl.hostname !== 'db.localtest.me';
+	neonConfig.wsProxy = (host) => (host === 'db.localtest.me' ? `${host}:4444/v2` : `${host}/v2`);
+}
+
+const sql = neon(connectionString);
+const db = drizzle(sql as any);
 
 export default db;
