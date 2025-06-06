a6a50b247089ea4fdf2ab0d177a2239d95dcab45
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2025-06-07 05:13:23 +1000

    fix

--- utils/auth.ts
+++ utils/auth.ts
@@ -5,7 +5,6 @@ console.log("Initializing Auth0 client...", process.env.AUTH0_REDIRECT_URI);
 export const auth0 = new Auth0Client({
   authorizationParameters: {
     scope: process.env.AUTH0_SCOPE,
-    audience: process.env.AUTH0_AUDIENCE,
     redirect_uri: process.env.AUTH0_REDIRECT_URI,
   }
 });
