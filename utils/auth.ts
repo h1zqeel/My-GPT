91678ac6d4bc00a80bea121e1fe4d228149075bc
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2025-06-07 04:40:25 +1000

    fix

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,6 +1,5 @@
 
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
-import { console } from "inspector";
 
 console.log("Initializing Auth0 client...", process.env.AUTH0_REDIRECT_URI);
 export const auth0 = new Auth0Client({
